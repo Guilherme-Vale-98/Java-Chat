a914cef28b5511bde1ba5325d0fc2e8cfa20eb07
Guilherme Vale <guilherme.vale98@gmail.com>
2024-05-09 09:38:05 -0300

    add Kafka Producer configuration

--- src/main/java/com/gui/javakafkachat/controllers/MessageController.java
+++ src/main/java/com/gui/javakafkachat/controllers/MessageController.java
@@ -1,11 +1,34 @@
 package com.gui.javakafkachat.controllers;
 
 import com.gui.javakafkachat.models.Message;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.concurrent.ExecutionException;
 
 @RestController
 public class MessageController {
-
+    @Autowired
     private KafkaTemplate<String, Message> kafkaTemplate;
+    @PostMapping(value = "api/send",
+            consumes = "application/json",
+            produces = "application/json")
+    public void sendMessage(@RequestBody Message message){
+        LocalDateTime dateObject = LocalDateTime.now();
+        DateTimeFormatter myFormat = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+        message.setTimestamp(dateObject.format(myFormat));
+
+        try {
+            kafkaTemplate.send(KAFKA_CHAT_TOPIC, message).get();
+        }catch (InterruptedException | ExecutionException e){
+            throw new RuntimeException(e);
+        }
+
+    }
+
 }
---
+++ src/main/java/com/gui/javakafkachat/kafka/KafkaProducerConfig.java
@@ -0,0 +1,38 @@
+package com.gui.javakafkachat.kafka;
+
+
+import com.gui.javakafkachat.models.Message;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonSerializer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@EnableKafka
+@Configuration
+public class KafkaProducerConfig {
+    private static final String BOOTSTRAPS_SERVERS = "localhost:9092,localhost:9093,localhost:9094,";
+    @Bean
+    public ProducerFactory<String, Message> producerFactory(){
+        Map<String, Object> configs = new HashMap<>();
+
+        configs.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAPS_SERVERS);
+        configs.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        configs.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+
+        return new DefaultKafkaProducerFactory<>(configs);
+    }
+
+
+    @Bean
+    public KafkaTemplate<String, Message> kafkaTemplate(){
+        return new KafkaTemplate<>(producerFactory());
+    }
+}
